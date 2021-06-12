42124a5bdf4aded97cd7ef6cdde5ad7a8b58e8df
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2021-06-12 19:33:41 +0200

    Handle Category and routing

---
+++ src/app/components/product-category/product-category.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CategoryService } from '../../shared/services/category/category.service';
+
+@Component({
+  selector: 'app-product-Category',
+  templateUrl: './product-category.component.html',
+  styleUrls: ['./product-category.component.scss'],
+})
+export class ProductCategoryComponent implements OnInit {
+  category$: any;
+  @Input("category") category: string;
+
+  constructor(private categoryService: CategoryService) {}
+  ngOnInit() {
+    this.category$ =  this.categoryService.getCategoryies();
+  }
+}
--- src/app/components/product/product.module.ts
+++ src/app/components/product/product.module.ts
@@ -1,4 +1,5 @@
 import { ProductComponent } from './../product/product.component';
+import { ProductCategoryComponent } from './../product-category/product-category.component';
 import { ProductCartComponent } from './../product-cart/product-cart.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,11 @@ import { QuickViewComponent } from '../quick-view/quick-view.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, ProductRoutingModule],
-  declarations: [ProductComponent, ProductCartComponent, QuickViewComponent],
+  declarations: [
+    ProductComponent,
+    ProductCartComponent,
+    QuickViewComponent,
+    ProductCategoryComponent
+  ],
 })
-export class ProductModule {} 
+export class ProductModule {}
