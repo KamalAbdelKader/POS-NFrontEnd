7958969b0d1ac1181ca5d75c97bbb7f9ae51a270
Kamal Abd El-Kader <kamal.abdel-kader@ibnsina-pharma.com>
2021-06-13 02:37:42 +0200

    Show item image istead of static one // commented to be replanced

--- src/app/components/product-cart/product-cart.component.ts
+++ src/app/components/product-cart/product-cart.component.ts
@@ -1,6 +1,12 @@
-import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { Item } from 'src/app/shared/model/item';
-import { getImageUrl } from "src/app/shared/helper/image";
+import { getImageUrl } from 'src/app/shared/helper/image';
 import { QuickViewComponent } from '../quick-view/quick-view.component';
 
 @Component({
@@ -10,20 +16,17 @@ import { QuickViewComponent } from '../quick-view/quick-view.component';
 })
 export class ProductCartComponent implements OnInit {
   category$: any;
-  @ViewChild("quickView", { static: false }) QuickView: TemplateRef<any>;
-  @Input("item") item: Item;
-  imageUrl: string | ArrayBuffer = "";
-  
+  @ViewChild('quickView', { static: false }) QuickView: TemplateRef<any>;
+  @Input('item') item: Item;
+  imageUrl: string | ArrayBuffer = '';
+
   constructor() {}
   ngOnInit() {
-    this.imageUrl = "data:image/png;base64," + this.item["pic_1"];
+    this.imageUrl = 'data:image/png;base64,' + this.item['pic_1'];
   }
-
 
   get_ImageUrl(item: Item) {
     return getImageUrl(item);
+    // return 'data:image/png;base64,' + item.image;
   }
-
 }
-
-
