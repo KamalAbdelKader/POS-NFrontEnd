4754f5d1d5b6f2a6cadd6bd9b40f45533fa0c58f
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2021-06-13 02:45:25 +0200

    checkout

--- src/app/components/product-checkout/product-checkout.component.ts
+++ src/app/components/product-checkout/product-checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { getImageUrl } from 'src/app/shared/helper/image';
+import { Item } from 'src/app/shared/model/item';
+import { ShoppingCartService } from 'src/app/shared/services/shopping.service';
 
 @Component({
   selector: 'app-product-checkout',
@@ -6,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-checkout.component.scss'],
 })
 export class ProductCheckoutComponent implements OnInit {
-  
-  constructor() {}
+  items: Item[] = [];
+  constructor(
+    private shoppingCartService: ShoppingCartService
+  ) {}
   ngOnInit() {
+    this.shoppingCartService.currentItemsList.subscribe((items) => {
+      this.items = items.map((item) => {
+        item.image = "data:image/png;base64," + item["pic_1"];
+        return item;
+      });
+    });
+  }
+
+
+  get_ImageUrl(item: Item) {
+    return getImageUrl(item);
+  }
+
+  getTotalPrice() {
+    return this.shoppingCartService.getTotalPrice();
   }
 }
