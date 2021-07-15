b0f6e7294f3bd96a3d829ef96a4d433e45c2b534
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2021-07-15 05:09:15 +0200

    Add loader

--- src/app/components/loader/loaderSpinner.component.ts
+++ src/app/components/loader/loaderSpinner.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'LoaderSpinner',
-  template: `<div class="loader">Loading...</div>`,
+  template: `<div class="justify-content-center">
+    <div class="loader"></div>
+  </div>`,
   styleUrls: ['./loaderSpinner.component.scss'],
 })
 export class LoaderSpinnerComponent {}
