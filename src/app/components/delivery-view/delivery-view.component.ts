25854a3d6f5a79c9a6808a37e1538f5135643e15
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2021-09-08 02:23:00 +0200

    Handle delivery desgin

--- src/app/components/delivery-view/delivery-view.component.ts
+++ src/app/components/delivery-view/delivery-view.component.ts
@@ -1,30 +1,47 @@
 import { LayoutService } from '../../shared/services/layout.service';
+import { Component, OnInit } from '@angular/core';
 import {
-  Component,
-  OnInit
-} from '@angular/core';
-import { NgbModal, ModalDismissReasons, NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+  NgbModal,
+  ModalDismissReasons,
+  NgbTabChangeEvent,
+} from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-delivery-view',
   templateUrl: './delivery-view.component.html',
   styleUrls: ['./delivery-view.component.scss'],
 })
-
 export class DeliveryViewComponent implements OnInit {
-   // tslint:disable-next-line:no-input-rename
 
   public closeResult: string;
   public modalOpen = false;
-  
+  form: FormGroup = new FormGroup({});
   currentJustify = 'start';
   currentOrientation = 'horizontal';
-  
 
-  constructor(public layout: LayoutService, private modalService: NgbModal) {}
+  constructor(public layout: LayoutService) {}
 
-  ngOnInit(): void {}
- 
+  ngOnInit(): void {
+    this.form = this.CreateForm();
+  }
+
+  CreateForm(): FormGroup {
+    return new FormGroup({
+          Name: new FormControl(''),
+          mail: new FormControl(''),
+          Number: new FormControl(''),
+          block: new FormControl(''),
+          street: new FormControl(''),
+          building: new FormControl(''),
+          floor: new FormControl(''),
+          type: new FormControl(''),
+          car: new FormControl(''),
+          color: new FormControl(''),
+          carNumber: new FormControl('')
+    });
+  }
+
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
@@ -33,11 +50,14 @@ export class DeliveryViewComponent implements OnInit {
     } else {
       return `with: ${reason}`;
     }
-  } 
-  
-  public beforeChange($event: NgbTabChangeEvent) {
-      if ($event.nextId === 'tab-preventchange2') {
-        $event.preventDefault();
-      }
   }
+
+  public beforeChange($event: NgbTabChangeEvent): void {
+    if ($event.nextId === 'tab-preventchange2') {
+      $event.preventDefault();
+    }
+  }
+
+
+
 }
